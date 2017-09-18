68e7ad938a3657d1e30205a9d9468563a2035afc
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-18 15:14:27 +0200

    Added check if number is even

--- js/project.js
+++ js/project.js
@@ -1,18 +1,31 @@
 (function() {
   var evenElements = selectAll("ul[data-show-oddeven] li");
-  var counter = 0;
+  var counter = 1;
   evenElements.forEach((function(element, index, nodeList) {
 
-    if (counter == 0) {
+    if (checkIfNumberIsEven(counter) == true) {
       // Odd element
-      element.title = "Odd";
-      counter++;
+      element.title = "Even";
     }
 
     else {
       // Even element
-      element.title = "Even";
-      counter--;
+      element.title = "Odd";
+    }
+    counter++;
+
+    function checkIfNumberIsEven(number) {
+      var result = number / 2;
+      if (Number.isInteger(result) == true) {
+        // It is even
+        return(true);
+      }
+
+      else {
+        // It isn't even
+        return(false);
+      }
+      console.log(" ");
     }
   }));
 })();
